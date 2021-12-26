189db8f386be16ec3d43442b53eebc9d7d90462c
juan-c-s <juancamilo.salazar2002@gmail.com>
2021-12-25 22:39:47 -0500

    v

--- src/index.js
+++ src/index.js
@@ -60,7 +60,7 @@ io.on('connection', (socket) => {
         const filteredMSG = filter.clean(message)
         io.to(user.room).emit('msg', generateMessage(user.username, filteredMSG))//io.emit sends to all users
     })
-
+    ///2
     socket.on('sendLocation', (coords, callback) => {
         const user = getUser(socket.id)
         io.to(user.room).emit('locationMessage', generateLocationMessage(user.username, `https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
